16826fd565775b2dd0c3f4b7513ad6bd54d7cd69
canhvd <canhvd@venesa.vn>
2025-12-08 10:37:19 +0700

    update global exception handler

--- src/main/java/com/example/demo1/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo1/exception/GlobalExceptionHandler.java
@@ -2,27 +2,27 @@ package com.example.demo1.exception;
 
 import com.example.demo1.response.BaseResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @ControllerAdvice // Chỉ định lớp này xử lý ngoại lệ chung
 @Slf4j
 public class GlobalExceptionHandler<T> {
 
-    @ExceptionHandler(Exception.class)
-    public ResponseEntity<BaseResponse<T>> handleGeneralException(Exception exception) {
-        log.error("Exception cause = {}", exception.getMessage());
-        BaseResponse<T> response = BaseResponse.<T>builder().code(1).msg(exception.getMessage()).build();
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
+    @ResponseBody
     @ExceptionHandler(BindingResultException.class)
-    public ResponseEntity<?> handleResourceNotFoundException(BindingResultException exception) {
+    public BaseResponse<T> handleResourceNotFoundException(BindingResultException exception) {
         log.error("Exception cause = {}", exception.getMessage());
         BaseResponse<T>  response = BaseResponse.<T>builder().code(2).msg(exception.getMessage()).build();
-        return new ResponseEntity<>(response, HttpStatus.OK);
+        return response;
     }
 
+    @ResponseBody
+    //@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public BaseResponse<T> handleGeneralException(Exception exception) {
+        log.error("Exception cause = {}", exception.getMessage());
+        return BaseResponse.<T>builder().code(1).msg(exception.getMessage()).build();
+    }
 }
